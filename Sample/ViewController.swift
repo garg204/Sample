80fe94c752beabda646f77dfb623ef6a993a550d
Ankit Garg <>
2018-05-13 23:34:48 +0530

    removed code

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -10,18 +10,5 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-//Hello
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 }
 
